d7d8c1d39534b17492b86f2539aa04485d217f2b
jyl-jyl <97498760+jyl-jyl@users.noreply.github.com>
2022-05-04 20:47:29 -0400

    Added markers for all sites on individual show page

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,13 +1,20 @@
 
-const populateMarker =  () => {
-	arrayOfDocsHist.forEach( (e) => {
-		let popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`<h5>${e.name}</h5><p>${e.address}</p>`);
-		new mapboxgl.Marker()
-		.setLngLat(e.geometry.coordinates)
-		.setPopup(popup)
-		.addTo(map);
-	});
+	const populateMarker =  () => {
+		for (let object of arrayOfDocsHist) {
+			const coor = object.geometry.coordinates;
+			const name = object.name;
+			const address = object.address;
+			const id = object._id;
+			const html = `<a href="/historicsites/${id}"><h5>${name}</a></h5><p>${address}</p>`;
+			let popup = new mapboxgl.Popup({ offset: 25 }).setHTML(html);
+			new mapboxgl.Marker()
+			.setLngLat(coor)
+			.setPopup(popup)
+			.addTo(map);
+		}
+
 };
+
 
 
 
@@ -19,7 +26,7 @@ center: historicsite.geometry.coordinates, // starting position [lng, lat]
 zoom: 15 // starting zoom
 });
 
-// populateMarker();
+populateMarker();
 
 // create the popup for current site
 const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
