64e1e77eaaa698f0206f19c4c70ae4c60659be6e
jyl-jyl <97498760+jyl-jyl@users.noreply.github.com>
2022-05-05 01:10:03 -0400

    Added start script for heroku

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error', { err });
 })
 
-app.listen(5000, () => {
-	console.log('SERVING ON PORT 5000 ...');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`SERVING ON PORT ${port} ...`);
 })
