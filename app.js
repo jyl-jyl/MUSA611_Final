1226911e3c1fd664a1e840bd9d7a19c42c91c28e
jyl-jyl <97498760+jyl-jyl@users.noreply.github.com>
2022-05-05 10:54:41 -0400

    Changed back to use mongo atlas

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const flash = require('connect-flash');
 
 
 // const dbUrl = process.env.DB_URL;
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/7th-ward';
-const dbUrl = 'mongodb://localhost:27017/7th-ward';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/7th-ward';
+// const dbUrl = 'mongodb://localhost:27017/7th-ward';
 
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
